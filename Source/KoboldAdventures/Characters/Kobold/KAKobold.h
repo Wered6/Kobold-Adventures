ca26ce2bfa37b249b3547bff5194cf8be75cc455
Arek <arek0006@gmail.com>
2024-05-05 12:06:47 +0200

    NEW: Added inputs, Kobold can move and do attack animations with simple combo

---
+++ Source/KoboldAdventures/Characters/Kobold/KAKobold.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "KAKobold.generated.h"
+
+UCLASS()
+class KOBOLDADVENTURES_API AKAKobold : public APaperZDCharacter
+{
+	GENERATED_BODY()
+};
